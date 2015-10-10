324f78a537796d23374a271d339e22203ac7cfe9
Marc Henklein <snooze82@gmail.com>
2015-10-10 12:41:14 +0200

    basic threejs renderer

--- blinkspiel.js
+++ blinkspiel.js
@@ -9,7 +9,41 @@
 		gameState: gameStates.WAITINGFORDEVICE,
 		init: function() {
 
-			// 
+			var WIDTH = window.innerWidth;
+			var HEIGHT = window.innerHeight;
+
+			var NEAR = 0.1;
+			var FAR = 5000;
+			var FOV = 45;
+
+			this.gamecanvas = document.getElementById('blinkspiel');
+			this.renderer = new THREE.WebGLRenderer({canvas: this.gamecanvas});
+
+			this.renderer.setSize(WIDTH, HEIGHT);
+			this.renderer.shadowMap.enabled = true;
+			this.renderer.shadowMapSoft = true;
+			this.renderer.setClearColor(0x402020, 1);
+			this.renderer.autoClear = false;
+
+			this.renderer.gammaInput = true;
+			this.renderer.gammaOutput = false;
+
+			this.scene = new THREE.Scene();
+
+			this.camera = new THREE.PerspectiveCamera(FOV, WIDTH/ HEIGHT, NEAR, FAR);
+			this.camera.position.set(0,50,0);
+			this.scene.add(this.camera);
+
+			var test = new THREE.SphereGeometry(10,10, 32);
+			var material = new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.DoubleSide});
+			var mesh = new THREE.Mesh(test, material);
+			this.scene.add(mesh);
+			this.camera.lookAt(mesh.position);
+
+
+			this.animate();
+
+
 			chrome.hid.getDevices({}, Blinkspiel.onDevicesEnumerated);
 			if (chrome.hid.onDeviceAdded) {
 				chrome.hid.onDeviceAdded.addListener(Blinkspiel.onDeviceAdded);
@@ -53,9 +87,17 @@
 			this.gameState = state;
 			switch (this.gameState) {
 				case gameStates.READYPLAYERONE:
-				bg.blink1.fadeRgb(60, 128, 200, 2500, 0);
+				//bg.blink1.fadeRgb(60, 128, 200, 2500, 0);
+				bg.blink1.fadeRgb(0, 0, 0, 2500, 0);
 				break;
 			}
+		},
+		render: function() {
+			Blinkspiel.renderer.render(Blinkspiel.scene, Blinkspiel.camera);
+		},
+		animate: function() {
+			requestAnimationFrame(Blinkspiel.animate);
+			Blinkspiel.render();
 		}
 	}
 
